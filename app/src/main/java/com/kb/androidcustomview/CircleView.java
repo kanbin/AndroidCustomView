83d1cfede1bc4bec649a4a4c9d7907c058761e0e
kanbin <kanbin@live.com>
2017-02-06 16:54:05 +0800

    CircleView

---
+++ app/src/main/java/com/kb/androidcustomview/CircleView.java
@@ -0,0 +1,74 @@
+package com.kb.androidcustomview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/2/6.
+ */
+public class CircleView extends View {
+
+    private int mColor = Color.BLUE;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, Color.RED);
+        a.recycle();
+        init();
+
+    }
+
+    private void init() {
+        mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int defaultDimension = 200;
+
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(defaultDimension, defaultDimension);
+        } else if (widthSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(defaultDimension, heightMeasureSpec);
+        }else if (heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthMeasureSpec, defaultDimension);
+        }
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
+        int radius = Math.min(width, height) / 2;
+        canvas.drawCircle(width / 2 + paddingLeft, height / 2 + paddingTop, radius, mPaint);
+    }
+}
